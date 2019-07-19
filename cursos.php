a8aa298b8de5164032f05b022c6296b176c66c42
lucasweiby <lucas_weiby@hotmail.com>
2019-07-19 10:04:08 -0300

    Courses adjusts

--- cursos.php
+++ cursos.php
@@ -8,15 +8,31 @@
                     <h3 class="course-name">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, officiis?
                     </h3>
+                    <small class="course-info"><?php echo date("d/m/Y"); ?></small>
                 </div>
-                <div class="col-md-4">
+                <div class="col-md-4 course-data">
                     <div class="card">
-                        <img src="..." class="card-img-top" alt="...">
+                        <img src="https://images.unsplash.com/photo-1563372027-854b1d247b4a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1352&q=80" class="card-img-top" alt="...">
                         <div class="card-body">
                             <h5 class="card-title">Card title</h5>
                             <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                             <a href="#" class="btn btn-primary">Go somewhere</a>
                         </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="course-body">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <h4 class="course-title">Instrutor</h4>
+                    <div class="row">
+                        <div class="col-md-4">
+                            <img src="https://cdn.business2community.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640.png" alt="" class="img-fluid rounded-circle">
+                        </div>
+                        <div class="col-md-8"></div>
                     </div>
                 </div>
             </div>
